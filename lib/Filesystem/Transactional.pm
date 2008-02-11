a98c895b44523ac6f2a14ffc5c2d3d675c2fde56
Jonathan Rockway <jon@jrock.us>
2008-02-11 00:17:37 -0600

    add some basic docs

--- lib/Filesystem/Transactional.pm
+++ lib/Filesystem/Transactional.pm
@@ -2,10 +2,31 @@ package Filesystem::Transactional;
 use strict;
 use warnings;
 
+1;
+__END__
+
 =head1 NAME
 
-Filesystem::Transactional - 
+Filesystem::Transactional - a FUSE filesystem that suports RDMBS transaction
+semantics.
 
-=cut
+=head1 SYNOPSIS
 
-1;
+  # XXX: mount the fileystem somehow
+
+  $ cd txnfs
+  $ touch .txn/begin
+  $ touch "foo" "bar"
+  $ ls
+  foo    bar
+  $ touch .txn/rollback
+  $ ls
+  # no files!
+
+=head1 DESCRIPTION
+
+  $ rm -rf /
+  # OH FUCK
+  $ touch /.txn/rollback
+  # DISASTER AVERTED
+
